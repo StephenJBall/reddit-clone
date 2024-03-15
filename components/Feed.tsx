d21c424901cabd589164f431b1672152e50d21f9
StephenJBall <stephenjb94@gmail.com>
2024-03-15 15:55:58 +0000

    Fixed querying issue for retrieving posts in feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,21 @@
+import { GET_ALL_POSTS } from "@/graphql/queries";
+import { useQuery } from "@apollo/client";
 import React from "react";
+import Post from "./Post";
 
 function Feed() {
-  return <div>Feed Goes Here</div>;
+  const { data, error } = useQuery(GET_ALL_POSTS);
+  console.log(data);
+
+  const posts: Post[] = data?.postList;
+
+  return (
+    <div>
+      {posts?.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
 }
 
 export default Feed;
--- graphql/queries.ts
+++ graphql/queries.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const GET_ALL_POSTS = gql`
   query MyQuery {
-    getPostList {
+    postList {
       body
       created_at
       id
@@ -10,25 +10,25 @@ export const GET_ALL_POSTS = gql`
       title
       subreddit_id
       username
-    }
-    comments {
-      created_at
-      id
-      post_id
-      text
-      username
-    }
-    subreddit {
-      created_at
-      id
-      topic
-    }
-    votes {
-      created_at
-      id
-      post_id
-      upvote
-      username
+      comment {
+        created_at
+        id
+        post_id
+        text
+        username
+      }
+      subreddit {
+        created_at
+        id
+        topic
+      }
+      vote {
+        created_at
+        id
+        post_id
+        upvote
+        username
+      }
     }
   }
 `;
