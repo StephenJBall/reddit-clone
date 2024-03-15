23646e35e583e5e6108e35f94430fa54c1fd2c09
StephenJBall <stephenjb94@gmail.com>
2024-03-15 16:08:01 +0000

    upvote downvote appearance added

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,3 +1,5 @@
+import { ArrowUpIcon } from "@heroicons/react/16/solid";
+import { ArrowDownIcon } from "@heroicons/react/24/solid";
 import React from "react";
 
 type Props = {
@@ -5,7 +7,20 @@ type Props = {
 };
 
 function Post({ post }: Props) {
-  return <div>Post</div>;
+  return (
+    <div>
+      <div>
+        <ArrowUpIcon className="voteButtons text-white hover:text-blue-600" />
+        <ArrowDownIcon className="voteButtons text-white hover:text-red-600" />
+      </div>
+      <div>
+        {/* Header */}
+        {/* Body */}
+        {/* Image */}
+        {/* Footer */}
+      </div>
+    </div>
+  );
 }
 
 export default Post;
