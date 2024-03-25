f0d61f8319623b3dad91e20af82b3b31cef4a5b0
StephenJBall <stephenjb94@gmail.com>
2024-03-25 09:25:53 +0000

    Added subreddit page initial structure

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -16,7 +16,11 @@ type FormData = {
   subreddit: string;
 };
 
-function PostBox() {
+type Props = {
+  subreddit?: string;
+};
+
+function PostBox({ subreddit }: Props) {
   const { data: session } = useSession();
   const [imageBoxOpen, setImageBoxOpen] = useState(false);
   const [addPost] = useMutation(ADD_POST, {
@@ -41,7 +45,7 @@ function PostBox() {
       } = await client.query({
         query: GET_SUBREDDIT_BY_TOPIC,
         variables: {
-          topic: formData.subreddit,
+          topic: subreddit || formData.subreddit,
         },
       });
 
@@ -114,7 +118,11 @@ function PostBox() {
           className="bg-gray-50 p-2 pl-5 outline-none rounded-md flex-1 text-black"
           type="text"
           placeholder={
-            session ? "Create a post by entering a title!" : "Sign in to Post"
+            session
+              ? subreddit
+                ? `Create a post in r/${subreddit}`
+                : "Create a post by entering a title!"
+              : "Sign in to Post"
           }
         />
         <PhotoIcon
@@ -137,15 +145,17 @@ function PostBox() {
               placeholder="Text (Optional)"
             />
           </div>
-          <div className="flex items-center px-2">
-            <p className="min-w-[90px]">Subreddit</p>
-            <input
-              className="m-2 flex-1 bg-blue-50 p-2 outline-none text-black rounded-md"
-              {...register("subreddit", { required: true })}
-              type="text"
-              placeholder="The community you want to post it to..."
-            />
-          </div>
+          {!subreddit && (
+            <div className="flex items-center px-2">
+              <p className="min-w-[90px]">Subreddit</p>
+              <input
+                className="m-2 flex-1 bg-blue-50 p-2 outline-none text-black rounded-md"
+                {...register("subreddit", { required: true })}
+                type="text"
+                placeholder="The community you want to post it to..."
+              />
+            </div>
+          )}
           <div>
             {imageBoxOpen && (
               <div className="flex items-center px-2">
---
+++ pages/subreddit/[topic].tsx
@@ -0,0 +1,34 @@
+import Avatar from "@/components/Avatar";
+import Feed from "@/components/Feed";
+import PostBox from "@/components/PostBox";
+import { useRouter } from "next/router";
+import React from "react";
+
+function Subreddit() {
+  const {
+    query: { topic },
+  } = useRouter();
+  return (
+    <div className={`h-24 bg-gray-400 p-8`}>
+      <div className="-mx-8 mt-10 bg-gray-500">
+        <div className="mx-auto flex max-w-5xl items-center space-x-4 pb-3">
+          <div className="-mt-5">
+            <Avatar seed={topic as string} />
+          </div>
+          <div className="py-2">
+            <h1 className="text-3xl font-semibold">
+              Welcome to the r/{topic} subreddit
+            </h1>
+            <p className="text-sm text-gray-200">r/{topic}</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <PostBox subreddit={topic as string} />
+        <Feed />
+      </div>
+    </div>
+  );
+}
+
+export default Subreddit;
