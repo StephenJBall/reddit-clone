238b3f45c895b5a11e954440be0069611b87b493
StephenJBall <stephenjb94@gmail.com>
2024-03-26 09:28:00 +0000

    Links implementation on subreddit

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,20 +16,23 @@ import {
   VideoCameraIcon,
 } from "@heroicons/react/24/outline";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 function Header() {
   const { data: session } = useSession();
   return (
     <div className="sticky top-0 z-50 flex bg-black px-4 py-2 shadow-lg shadow-gray-500 items-center">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
-        <Image
-          src="/images/reddit-icon.png"
-          height={40}
-          width={40}
-          style={{
-            cursor: "pointer",
-          }}
-        />
+        <Link href="/">
+          <Image
+            src="/images/reddit-icon.png"
+            height={40}
+            width={40}
+            style={{
+              cursor: "pointer",
+            }}
+          />
+        </Link>
       </div>
 
       <div className="mx-7 flex items-center xl:min-1-[300px]">
--- components/Post.tsx
+++ components/Post.tsx
@@ -12,6 +12,7 @@ import {
 import React from "react";
 import Avatar from "./Avatar";
 import TimeAgo from "react-timeago";
+import Link from "next/link";
 
 type Props = {
   post: Post;
@@ -19,55 +20,59 @@ type Props = {
 
 function Post({ post }: Props) {
   return (
-    <div className="flex cursor-pointer rounded-md border-gray-300 bg-gray-700 shadow-sm hover:border hover:border-gray-600">
-      <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md p-4 text-gray-400">
-        <ArrowUpIcon className="voteButtons text-white hover:text-blue-600" />
-        <p className="text-white font-bold text-xs">0</p>
-        <ArrowDownIcon className="voteButtons text-white hover:text-red-600" />
+    <Link href={`/post/${post.id}`}>
+      <div className="flex cursor-pointer rounded-md border-gray-300 bg-gray-700 shadow-sm hover:border hover:border-gray-600">
+        <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md p-4 text-gray-400">
+          <ArrowUpIcon className="voteButtons text-white hover:text-blue-600" />
+          <p className="text-white font-bold text-xs">0</p>
+          <ArrowDownIcon className="voteButtons text-white hover:text-red-600" />
+        </div>
+        <div className="p-3 pb-1">
+          {/* Header */}
+          <div className="flex items-center space-x-2">
+            <Avatar seed={post.subreddit[0]?.topic} />
+            <p className="text-xs text-gray-400">
+              <Link href={`/subreddit/${post.subreddit?.topic}`}>
+                <span className="font-bold text-white hover:text-blue-400">
+                  r/{post.subreddit.topic}
+                </span>
+              </Link>
+              Posted by u/
+              {post.username} <TimeAgo date={post.created_at} />
+            </p>
+          </div>
+          {/* Body */}
+          <div className="py-4">
+            <h2 className="text-xl font-semibold">{post.title}</h2>
+            <p className="mt-2 text-sm font-light">{post.body}</p>
+          </div>
+          {/* Image */}
+          <img src={post.image} className="width-full rounded-md" />
+          {/* Footer */}
+          <div className="flex space-x-4 text-gray-400">
+            <div className="postButtons">
+              <ChatBubbleBottomCenterIcon className="h-6 w-6" />
+              <p className="">{post.comment?.length}</p>
+            </div>
+            <div className="postButtons">
+              <GiftIcon className="h-6 w-6" />
+              <p className="hidden sm:inline">Award</p>
+            </div>
+            <div className="postButtons">
+              <ShareIcon className="h-6 w-6" />
+              <p className="hidden sm:inline">Share</p>
+            </div>
+            <div className="postButtons">
+              <BookmarkIcon className="h-6 w-6" />
+              <p className="hidden sm:inline">Save</p>
+            </div>
+            <div className="postButtons">
+              <EllipsisHorizontalIcon className="h-6 w-6" />
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="p-3 pb-1">
-        {/* Header */}
-        <div className="flex items-center space-x-2">
-          <Avatar seed={post.subreddit[0]?.topic} />
-          <p className="text-xs text-gray-400">
-            <span className="font-bold text-white hover:text-blue-400">
-              r/{post.subreddit.topic}
-            </span>{" "}
-            Posted by u/
-            {post.username} <TimeAgo date={post.created_at} />
-          </p>
-        </div>
-        {/* Body */}
-        <div className="py-4">
-          <h2 className="text-xl font-semibold">{post.title}</h2>
-          <p className="mt-2 text-sm font-light">{post.body}</p>
-        </div>
-        {/* Image */}
-        <img src={post.image} className="width-full rounded-md" />
-        {/* Footer */}
-        <div className="flex space-x-4 text-gray-400">
-          <div className="postButtons">
-            <ChatBubbleBottomCenterIcon className="h-6 w-6" />
-            <p className="">{post.comment?.length}</p>
-          </div>
-          <div className="postButtons">
-            <GiftIcon className="h-6 w-6" />
-            <p className="hidden sm:inline">Award</p>
-          </div>
-          <div className="postButtons">
-            <ShareIcon className="h-6 w-6" />
-            <p className="hidden sm:inline">Share</p>
-          </div>
-          <div className="postButtons">
-            <BookmarkIcon className="h-6 w-6" />
-            <p className="hidden sm:inline">Save</p>
-          </div>
-          <div className="postButtons">
-            <EllipsisHorizontalIcon className="h-6 w-6" />
-          </div>
-        </div>
-      </div>
-    </div>
+    </Link>
   );
 }
 
